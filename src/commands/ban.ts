bfa14fa60ba29defbbccafe156fb735b333f7f6c
XzFrosT <mschodeihsan@gmail.com>
2022-05-17 12:52:41 +0700

    ban command

---
+++ src/commands/ban.ts
@@ -0,0 +1,47 @@
+import { InteractionResponseType } from 'discord-interactions';
+
+import { CommandInterface } from "../utils/command";
+import request from "../utils/request";
+
+export default <CommandInterface>{
+	name: "ban",
+	name_localizations: {},
+	description: "Ban a member from this server",
+	description_localizations: {},
+	options: [
+		{
+			type: 6,
+			name: "member",
+			name_localizations: {},
+			description: "@member or their id to ban",
+			description_localizations: {},
+			required: true
+		},
+		{
+			type: 3,
+			name: "duration",
+			name_localizations: {},
+			description: "Duration of the ban. the member will be unbanned after the given duration.",
+			description_localizations: {},
+			required: false
+		},
+		{
+			type: 3,
+			name: "reason",
+			name_localizations: {},
+			description: "Reason of the ban",
+			description_localizations: {},
+			required: false
+		}
+	],
+	execute: async (req: any, res: any) => {
+		console.log(req.body);
+		
+		return res.send({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			data: {
+				content: "Banned!"
+			}
+		})
+	}
+}

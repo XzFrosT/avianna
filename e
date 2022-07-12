1d5b1fb59e59242b1b056640614288ed93ad6d76
XzFrosT <mschodeihsan@gmail.com>
2022-07-12 17:39:30 +0700

    removed unused  files

---
+++ e
@@ -0,0 +1,1 @@
+cp -rT $HOME/app/avianna/dist $HOME/storage/shared/Android/data/io.spck/files/avianna/dist
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export const parseOptions = (req: any, target: string) => {
-	const option = req.body.data.options.filter((opt: any) => opt.name === target)[0];
-	
-	return option.value;
-}
